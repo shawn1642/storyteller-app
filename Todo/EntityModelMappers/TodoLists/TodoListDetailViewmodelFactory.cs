c67b834e85ef37f7a280949f0be98d638ac9a63c
Shawn <shawnm@apexbi.co.za>
2025-05-05 21:37:43 +0200

    feat: sort TodoItems by importance in detail view (High > Medium > Low)

--- Todo/EntityModelMappers/TodoLists/TodoListDetailViewmodelFactory.cs
+++ Todo/EntityModelMappers/TodoLists/TodoListDetailViewmodelFactory.cs
@@ -9,7 +9,11 @@ namespace Todo.EntityModelMappers.TodoLists
     {
         public static TodoListDetailViewmodel Create(TodoList todoList)
         {
-            var items = todoList.Items.Select(TodoItemSummaryViewmodelFactory.Create).ToList();
+            var items = todoList.Items
+                .Select(TodoItemSummaryViewmodelFactory.Create)
+                .OrderBy(i => i.Importance == Importance.High ? 0 : i.Importance == Importance.Medium ? 1 : 2)
+                .ToList();
+
             return new TodoListDetailViewmodel(todoList.TodoListId, todoList.Title, items);
         }
     }
